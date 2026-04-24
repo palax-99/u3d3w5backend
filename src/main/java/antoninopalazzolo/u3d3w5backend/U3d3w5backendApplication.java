c7562427ef937cdc3b05821359118e25112467c5
palax-99 <ninociao7@gmail.com>
2026-04-24 12:28:30 +0200

    Initial commit

---
+++ src/main/java/antoninopalazzolo/u3d3w5backend/U3d3w5backendApplication.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.u3d3w5backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U3d3w5backendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U3d3w5backendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antoninopalazzolo/u3d3w5backend/U3d3w5backendApplicationTests.java
@@ -0,0 +1,13 @@
+package antoninopalazzolo.u3d3w5backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U3d3w5backendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

fcb8a4e79a93dab594619a4f11105cc86008b046
palax-99 <ninociao7@gmail.com>
2026-04-24 13:03:58 +0200

    aggiunta entità User, Event, Booking

---
+++ src/main/java/antoninopalazzolo/u3d3w5backend/entities/Role.java
@@ -0,0 +1,5 @@
+package antoninopalazzolo.u3d3w5backend.entities;
+
+public enum Role {
+    USER, ORGANIZER, ADMIN
+}
---
+++ src/main/java/antoninopalazzolo/u3d3w5backend/entities/User.java
@@ -0,0 +1,58 @@
+package antoninopalazzolo.u3d3w5backend.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
+@JsonIgnoreProperties({"accountNonExpired", "accountNonLocked", "authorities", "credentialsNonExpired", "enabled"})
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue
+    @Setter(AccessLevel.NONE)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    @JsonIgnore
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public User(String name, String surname, String email, String password) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+        this.role = Role.USER;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+}
